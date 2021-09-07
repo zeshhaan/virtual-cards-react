4dc5dede9af1e3223f9197ebbfe98e33f7a7ab3f
Mohammed Zeeshan <34814964+zeeshanofficial@users.noreply.github.com>
2021-09-07 14:32:05 +0530

    added routing

--- components/Nav/Tabs.js
+++ components/Nav/Tabs.js
@@ -1,7 +1,9 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 const tabs = [
-  { name: "Your", href: "/your", current: false },
-  { name: "All", href: "/", current: true },
-  { name: "Blocked", href: "/blocked", current: false },
+  { name: "Your", path: "/your", current: false },
+  { name: "All", path: "/", current: true },
+  { name: "Blocked", path: "/blocked", current: false },
 ];
 
 function classNames(...classes) {
@@ -9,6 +11,7 @@ function classNames(...classes) {
 }
 
 export default function Tabs() {
+  const router = useRouter();
   return (
     <div>
       <div className="sm:hidden">
@@ -26,20 +29,20 @@ export default function Tabs() {
       <div className="hidden sm:block">
         <div className="border-b border-gray-200">
           <nav className="-mb-px flex space-x-8" aria-label="Tabs">
-            {tabs.map((tab) => (
-              <a
-                key={tab.name}
-                href={tab.href}
-                className={classNames(
-                  tab.current
-                    ? "border-pink-500 text-pink-600"
-                    : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
-                  "whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
-                )}
-                aria-current={tab.current ? "page" : undefined}
-              >
-                {tab.name}
-              </a>
+            {tabs.map((tab, index) => (
+              <Link key={index} href={tab.path}>
+                <a
+                  className={classNames(
+                    router.pathname === tab.path
+                      ? "border-pink-500 text-pink-600"
+                      : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
+                    "whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
+                  )}
+                  aria-current={tab.current ? "page" : undefined}
+                >
+                  {tab.name}
+                </a>
+              </Link>
             ))}
           </nav>
         </div>
