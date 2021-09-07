3a7e7ff75561c65670aee6b3cd96c72ef312cbac
Mohammed Zeeshan <34814964+zeeshanofficial@users.noreply.github.com>
2021-09-07 13:23:04 +0530

    add prod api path

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const apiUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000/api" // development api
-    : "http://localhost:3000/api"; // production api
+    : "https://virtual-cards-react.vercel.app/api"; // production api
 
 export { apiUrl };
